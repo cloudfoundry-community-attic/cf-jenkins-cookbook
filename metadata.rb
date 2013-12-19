7102b60333a3c0ed7d89cad618faf6aea85442f4
Ryan Grenz <info@ryangrenz.com>
2013-12-19 14:39:12 +0000

    Bumping version to 0.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'ryan.grenz@bskyb.com'
 license          'All rights reserved'
 description      'A build environment for Cloud Foundry v2 on Openstack'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.0.2'
 
 %w(jenkins user git_user apt ssh_known_hosts sudo).each { |cb| depends cb }
