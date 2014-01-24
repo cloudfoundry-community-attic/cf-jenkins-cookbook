d2372db4d4d481469e439303ce6da428995ec605
Ryan Grenz <info@ryangrenz.com>
2014-01-24 13:42:56 +0000

    Fix job creation and use :file_cache_path instead of /tmp

--- providers/job.rb
+++ providers/job.rb
@@ -1,12 +1,12 @@
 action :create do
-  tmp_config = "/tmp/#{new_resource.name}-config.xml"
-
-  jenkins_job new_resource.name do
-    config tmp_config
-  end
+  tmp_config = File.join(Chef::Config[:file_cache_path], "#{new_resource.name}-config.xml")
 
   template tmp_config do
     source (new_resource.template) ? new_resource.template : "#{new_resource.name}.xml"
     variables :vars => new_resource.config
   end   
+
+  jenkins_job new_resource.name do
+    config tmp_config
+  end
 end
