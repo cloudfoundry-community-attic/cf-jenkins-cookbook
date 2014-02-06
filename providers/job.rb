517e0c1fafb2806db1b09e52b1de240920489b7c
Ryan Grenz <info@ryangrenz.com>
2014-02-06 21:14:19 +0000

    Fixing job provider

--- providers/job.rb
+++ providers/job.rb
@@ -1,8 +1,8 @@
 action :create do
-  tmp_config = File.join(Chef::Config[:file_cache_path], "#{new_resource.name}-config.xml")
+  tmp_config = ::File.join(Chef::Config[:file_cache_path], "#{new_resource.name}-config.xml")
 
   template tmp_config do
-    source new_resource.template ?: "#{new_resource.name}.xml"
+    source new_resource.template ? new_resource.template : "#{new_resource.name}.xml"
     variables vars: new_resource.config
   end
 
