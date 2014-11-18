71bc5f9e9d46148fd155b44ddaeccc1bb72498e6
Ryan Grenz <ryan.grenz@bskyb.com>
2014-11-18 11:20:21 +0000

    Refactored job creation into build/deploy types allowing to be split into separate Jenkins boxes Reorganised Chef attributes for logical release and deployment configuration (example config to follow) Removed bats/stac2/vcap-yeti jobs, unfinished and irrelevant now. Newer acceptance tests suite jobs to follow.

--- recipes/jobs_component.rb
+++ recipes/jobs_component.rb
@@ -1,9 +1,12 @@
 # Install CF component jobs
-comp_jobs_install = node['jenkins_cf']['comp_jobs']
-if comp_jobs_install.count > 0
+jenkins_mode = node['jenkins_cf']['mode']
 
-  comp_jobs_install.each do |component|
-    log "Creating Jenkins job for #{component}"
-    include_recipe "jenkins_cf::jobs_component_#{component}"
-  end
+if jenkins_mode.include?('deploy')
+	comp_jobs_install = node['jenkins_cf']['comp_jobs']
+	if comp_jobs_install.count > 0
+	  comp_jobs_install.each do |component|
+	    log "Creating Jenkins job for #{component}"
+	    include_recipe "jenkins_cf::jobs_component_#{component}"
+	  end
+	end
 end
--- recipes/jobs_core.rb
+++ recipes/jobs_core.rb
@@ -1,25 +1,93 @@
-job_modes = node['jenkins_cf']['jobs']
+chef_gem 'chef-helpers'
+require 'chef-helpers'
 
-# Install core BOSH tgz/upload jobs
-%w{ cf-release cf-services-contrib-release bosh-release }.each do |bosh_release|
-  %w{ tgz upload }.each do |cycle|
-    job_name = "#{bosh_release}-#{cycle}"
-    job_config = node['jenkins_cf'].dup
-    job_config['mode'] = job_modes["#{job_name}"] || 'final'
-    jenkins_cf_job "#{job_name}" do
+config = node['jenkins_cf']
+jenkins_mode = config['mode']
+deployments = config['deployments']
+deployment_configs = deployments['config']  
+
+# BOSH release build/deploy jobs #################################################################################
+
+releases = config['releases']
+release_configs = releases['config']
+
+# Release Build jobs
+if jenkins_mode.include?('build')
+
+  releases['enabled'].each do |release_name|
+    job_name = "#{release_name}-release-tgz"
+    release_config = release_configs["#{release_name}"]
+
+    job_config = config.dup
+    job_config['release'] = {
+      name: release_name,
+      config: release_config
+    }
+
+    job_template = has_template?("#{job_name}.xml").nil? ? "generic-release-tgz.xml" : "#{job_name}.xml"
+
+    jenkins_cf_job job_name do
       config job_config
-      template "#{job_name}.xml"
-    end  
+      template job_template
+    end
   end
+
 end
 
-# Install core BOSH deploy jobs
-%w{ cf-deploy cf-contrib-deploy bosh-inner-deploy bosh-outer-deploy }.each do |deployment_job|
-  jenkins_cf_job "#{deployment_job}" do
-    config node['jenkins_cf']
-    template "#{deployment_job}.xml"
+# Release deployment-related jobs
+if jenkins_mode.include?('deploy')
+
+  deployments['enabled'].each do |deployment_name|
+    %w{upload deploy}.each do |stage|
+      deployment_config = deployment_configs[deployment_name]
+      target_bosh = deployment_config['bosh']
+      release_name = deployment_config['release'] || deployment_name
+      release_config = release_configs[release_name]
+
+      case stage
+        when 'upload'
+          next if deployment_name == 'bosh-outer'
+          job_name = "#{release_name}-release-#{target_bosh}-#{stage}"
+          job_desc = "Upload #{release_name} release to #{target_bosh} BOSH"
+        when 'deploy' 
+          job_name = "#{deployment_name}-#{stage}"
+          job_desc = "Deploy #{release_name} release through #{target_bosh} BOSH"
+      end
+
+      log "Creating #{job_name} (#{job_desc})"
+
+      cfg = {
+        job_name: job_name,
+        job_desc: job_desc,
+        deployment: {
+          name: deployment_name,
+          config: deployment_config,
+        },
+        release: {
+          name: release_name,
+          config: release_config,
+        },
+        bosh: {
+          name: target_bosh,
+          config: deployment_configs["bosh-#{target_bosh}"]
+        }
+      }
+
+      job_config = config.dup
+      job_config['_'] = cfg
+
+      job_template = has_template?("#{job_name}.xml").nil? ? "generic-release-#{stage}.xml" : "#{job_name}.xml"
+
+      jenkins_cf_job job_name do
+        config job_config
+        template job_template
+      end
+    end
   end
+
 end
+
+# BOSH stemcell build/deploy jobs ################################################################################
 
 # Stemcell tgz
 # Install inner/outer bosh stemcell upload jobs
@@ -28,34 +96,40 @@ downstream_jobs = {
   inner: 'cf-release-deploy',
 }
 
-stemcells = node['jenkins_cf']['stemcells']
+stemcells = config['stemcells']
 stemcells.each do |sc_name, sc_config|
-  stemcell_tgz_job = "stemcell-tgz-#{sc_name}"
 
-  stemcell_tgz_config = {
-    stemcell_base_url: sc_config['stemcell_base_url'],
-    stemcell: sc_config['stemcell']
-  }
+  if jenkins_mode.include?('build')
+    stemcell_tgz_job = "stemcell-tgz-#{sc_name}"
 
-  jenkins_cf_job stemcell_tgz_job do
-    config stemcell_tgz_config
-    template sc_config['mode'] == 'dev' ? 'stemcell-builder.xml' : 'stemcell-watcher.xml'
-  end
-
-  %w{ outer inner }.each do |bosh_layer|
-    bosh_config = node['jenkins_cf']["#{bosh_layer}_bosh"]
-
-    stemcell_upload_config = {
-      director_ip: bosh_config['director_ip'],
-      bosh_username: bosh_config['user'],
-      bosh_password: bosh_config['pass'],
-      downstream_jobs: downstream_jobs[bosh_layer.to_sym],
-      stemcell_artifact_source: stemcell_tgz_job
+    stemcell_tgz_config = {
+      stemcell_base_url: sc_config['stemcell_base_url'],
+      stemcell: sc_config['stemcell']
     }
 
-    jenkins_cf_job "stemcell-#{bosh_layer}-upload-#{sc_name}" do
-      config stemcell_upload_config
-      template 'stemcell-uploader.xml'
+    jenkins_cf_job stemcell_tgz_job do
+      config stemcell_tgz_config
+      template sc_config['mode'] == 'dev' ? 'stemcell-builder.xml' : 'stemcell-watcher.xml'
     end
   end
+
+  if jenkins_mode.include?('deploy')
+    %w{ outer inner }.each do |bosh_layer|
+      bosh_config = deployment_configs["bosh-#{bosh_layer}"]
+
+      stemcell_upload_config = {
+        director_ip: bosh_config['director_ip'],
+        bosh_username: bosh_config['user'],
+        bosh_password: bosh_config['password'],
+        downstream_jobs: downstream_jobs[bosh_layer.to_sym],
+        stemcell_artifact_source: stemcell_tgz_job
+      }
+
+      jenkins_cf_job "stemcell-#{bosh_layer}-upload-#{sc_name}" do
+        config stemcell_upload_config
+        template 'stemcell-uploader.xml'
+      end
+    end
+  end
+
 end
