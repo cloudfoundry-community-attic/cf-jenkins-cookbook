e565a49fdbb0da4690193fa4782188a02ce0b719
Ryan Grenz <ryan.grenz@bskyb.com>
2015-02-16 10:25:11 +0000

    Upgrade vagrant to 1.7.2

--- recipes/server.rb
+++ recipes/server.rb
@@ -105,3 +105,8 @@ template "/var/lib/jenkins/hudson.tasks.Shell.xml" do
   owner node['jenkins']['master']['user']
   group node['jenkins']['master']['user']
 end
+
+# vagrant setup
+node.set['vagrant']['url'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb'
+node.set['vagrant']['checksum'] = '9d7f1c587134011e2d5429eb21b6c0e95487f52e6d6d47c03ecc82cbeee73968'
+include_recipe 'vagrant'
